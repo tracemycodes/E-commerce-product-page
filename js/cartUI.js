aa5be7513bcd05eb75133bc0d173901a90e7b7fd
Agba Emmanuel <agbaayoh@gmail.com>
2022-03-20 06:36:19 -0700

    lightbox and carousel fully implemented

--- js/cartUI.js
+++ js/cartUI.js
@@ -2,6 +2,7 @@ class CartUI {
   constructor(count) {
     this.count = count;
     this.lightProductCarousel = document.querySelector('.light-product-carousel').children;
+    this.thumbnailIndex = document.querySelector('.thumbnail-Index').children;
   }
   updateCount(num) {
     this.count.textContent = num
@@ -12,11 +13,19 @@ class CartUI {
     this.count.textContent = 0
   }
   displayLightbox(index) {
-    const lightProductItems = [...this.lightProductCarousel]
+    let lightProductItems = [...this.lightProductCarousel]
     lightProductItems.forEach(productImg => {
       productImg.classList.remove('product-active')
     })
     // console.log(lightProductItems, index);
     lightProductItems[index].classList.add('product-active')
   }
-}
+  thumbnailBox(index) {
+    let thumbnailProductItems = [...this.thumbnailIndex]
+    thumbnailProductItems.forEach(productImg => {
+      productImg.classList.remove('product-active')
+    })
+    // console.log(lightProductItems, index);
+    thumbnailProductItems[index].classList.add('product-active')
+  }
+}thumbnail
